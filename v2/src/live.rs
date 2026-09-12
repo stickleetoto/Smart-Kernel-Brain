f68a7c2586b65a0dc8ba42eaef75cbf15f10e879
stockman <stickee090@gmail.com>
2026-09-12 10:11:47 +0900

    fix(v2): wire initial generation into live index tests

--- v2/src/live.rs
+++ v2/src/live.rs
@@ -1,4 +1,6 @@
-use crate::{GenerationEngine, LeanFileRefV2, ReloadError, ResolveError};
+use crate::{
+    GenerationEngine, LeanFileRefV2, ReloadError, ResolveError, INITIAL_GENERATION,
+};
 use serde::{Deserialize, Serialize};
 use skb::state::UsageState;
 use skb::{FileIndex, ResolvedFile};
